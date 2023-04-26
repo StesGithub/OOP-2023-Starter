ebf84f5e3d7a19cef67c4b9c4fd3977befcbab80
StesGithub <123895888+StesGithub@users.noreply.github.com>
2023-04-26 16:18:08 +0100

    Creation of Word and follow
    Word and Follow classes created, arrayList of type Follow in word,
    private int and string fields with public constructors in Follow

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,29 @@
+package ie.tudublin;
+
+public class Follow {
+    
+    private String word;
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+
+    public void setlCount(int count) {
+        this.count = count;
+    }
+
+
+    public String getWord() {
+        return word;
+    }
+
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    
+
+}
---
+++ java/src/ie/tudublin/Word.java
@@ -0,0 +1,11 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Word {
+    
+    ArrayList<Follow> followArrayList = new ArrayList<>();
+
+    
+
+}
